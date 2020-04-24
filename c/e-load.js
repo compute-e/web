6c23dfeb4b7653c65b8db0a156f476a5920def0b
Edward J <user-e@users.noreply.github.com>
2020-04-24 21:53:37 +0100

    edward.png, /c/jque...

--- c/e-load.js
+++ c/e-load.js
@@ -1,36 +1,23 @@
-function loadDoc(u, elem, f) {
-  var xhttp;
-  if (window.XMLHttpRequest) {
-    // code for modern browsers
-    xhttp = new XMLHttpRequest();
-  } else {
-    // IE6, IE5
-    xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      if (f == 2) {// js .replace all
-        f = this.responseText;
-        f = f.replace(new RegExp(
-          "<", 'g'), "&lt;");
-        elem.innerHTML = f.replace(new RegExp(
-          "\n", 'g'), "<br/>");
-      } else if (f) { f(this, elem);
-      } else {
-        elem.innerHTML = this.responseText;
-      }
-    }
-  };
-  xhttp.open("GET", u, true);
-  xhttp.send();
-};
+/* jquery load (html)
+*/
 var l = document.getElementsByClassName("e-load");
-var i; var f;
+var i;
 for (i = 0; i < l.length; i++) {
   var elem = l[i];
-  f = 0;
-  if (elem.getAttribute("data-rp") == "") {
-    f = 2;
-  }
-  loadDoc( elem.getAttribute("data-l"), elem, f );
+  /*loadDoc( elem.getAttribute("data-l"), elem, f );*/
+  $(elem).load(elem.getAttribute("data-l"), function(
+      text) {
+    var elem = this;
+    var f = 0;
+    if (elem.getAttribute("data-rp") == "") {
+      f = 2;
+    }
+    if (f == 2) {// js .replace all
+      f = elem.innerHTML;
+      f = f.replace(new RegExp(
+        "<", 'g'), "&lt;");
+      elem.innerHTML = f.replace(new RegExp(
+        "\n", 'g'), "<br/>");
+    } else if (f) { f(text, elem); }
+  });
 } // https://www.w3schools.com/jsref/jsref_statements.asp
